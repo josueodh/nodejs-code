97d2503ffb4476306f052b9712a5f25cb141bc1e
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-02-01 14:30:48 -0300

    Adicionando Readme.md e removendo comentários do controller

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -2,34 +2,6 @@ import { Request, Response } from "express";
 import { Product } from "models/Product";
 import { getRepository } from "typeorm";
 
-/**
- * GET - Buscar uma infomação dentro do servidor
- * POST - Inserir uma infomação no servidor
- * PUT - Alterar uma infomação no servidor
- * PATCH - Alterar uma infomação no servidor
- * DELETE - Deletar uma informação no servidor
- */
-
-/**
- *
- * Tipos de parâmetros
- *
- * Route Params => Identificar um recurso editar/deletar/buscar
- * Query Params => Paginação / Filtro
- * Body Params => Os objetos inserção/alteração (JSON)
- *
- */
-
-/*
- * Funções Controller
- *
- * Index => Listagem
- * Show => Detalhe
- * Delete => Deletar
- * Update => Atualizar
- * Create => Cadastrar
- */
-
 class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const productsRepository = getRepository(Product);
